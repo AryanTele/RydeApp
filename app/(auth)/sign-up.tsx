fe9fc2da80eff6228ba36c057703a15d61de1b0e
Aryan Tele <telearyan12@gmail.com>
2024-09-09 23:21:47 +0530

    start with sign-up page

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,23 @@
-import { View, Text } from "react-native";
+import { images } from "@/constants";
+import { View, Text, ScrollView, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignUp = () => {
   return (
-    <SafeAreaView>
-      <Text>SignUp</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px] " />
+          <Text className="text-black text-2xl font-JakartaBold absolute bottom-5 left-5">
+            Create your account
+          </Text>
+        </View>
+
+        <View className="py-5">
+          <InputField />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 export default SignUp;
